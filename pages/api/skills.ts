130fc70c44abbd0a30836963a71c1b99656b57aa
Paulo Barata <pbara7a@gmail.com>
2022-12-15 21:18:31 +0000

    Skills endpoint

--- pages/api/skills.ts
+++ pages/api/skills.ts
@@ -1,16 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { groq } from "next-sanity";
 import { sanityClient } from "../../sanity";
-import { Social } from "../../types";
+import { Skill } from "../../types";
 
-const query = groq`*[_type == "social"]`;
+const query = groq`*[_type == "skill"]`;
 
 type Data = {
-  socials: Array<Social>;
+  skills: Array<Skill>;
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const socials: Array<Social> = await sanityClient.fetch(query);
+  const skills: Array<Skill> = await sanityClient.fetch(query);
 
-  res.status(200).json({ socials });
+  res.status(200).json({ skills });
 }
