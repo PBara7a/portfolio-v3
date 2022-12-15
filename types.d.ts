2998cb384499064f6cb6000aafdf0512f59ae906
Paulo Barata <pbara7a@gmail.com>
2022-12-15 21:18:01 +0000

    Typing

--- types.d.ts
+++ types.d.ts
@@ -5,8 +5,33 @@ interface SanityBody {
   _updatedAt: string;
 }
 
+interface Image {
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: "reference";
+  };
+}
+
 export interface Social extends SanityBody {
   _type: "social";
   title: string;
   url: string;
+}
+
+export interface Skill extends SanityBody {
+  _type: "skill";
+  image: Image;
+  title: string;
+}
+
+export interface PageInfo extends SanityBody {
+  _type: "pageInfo"
+  heroImage:Image
+  name:string
+  role: string
+  aboutImage: Image
+  aboutTitle: string
+  aboutBody: string
+  socials: Array<Social>
 }
